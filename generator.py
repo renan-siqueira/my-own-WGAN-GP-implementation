2f5e7337b4b7f562931a9e539275f9880bf8ed61
Renan Siqueira Antonio <renan.siqu@gmail.com>
2023-09-08 13:38:14 -0300

    feat: Create a Generator architecture

---
+++ generator.py
@@ -0,0 +1,25 @@
+import torch.nn as nn
+
+class Generator(nn.Module):
+    def __init__(self, z_dim, channels_img, features_g):
+        super(Generator, self).__init__()
+        self.gen = nn.Sequential(
+            self._block(z_dim, features_g * 16, 4, 1, 0),
+            self._block(features_g * 16, features_g * 8, 4, 2, 1),
+            self._block(features_g * 8, features_g * 4, 4, 2, 1),
+            self._block(features_g * 4, features_g * 2, 4, 2, 1),
+            nn.ConvTranspose2d(features_g * 2, channels_img, 4, 2, 1),
+            nn.Tanh(),
+        )
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose2d(
+                in_channels, out_channels, kernel_size, stride, padding, bias=False
+            ),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2),
+        )
+
+    def forward(self, x):
+        return self.gen(x)
