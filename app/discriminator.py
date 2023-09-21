afd24e0f93fa4f5746ff1d33bcc3801d8c8800d9
Renan Siqueira Antonio <renan.siqu@gmail.com>
2023-09-21 09:29:30 -0300

    feat: Creation of a new parameter to specify whether it is a new traning or continue last training

--- app/discriminator.py
+++ app/discriminator.py
@@ -7,21 +7,17 @@ class Discriminator(nn.Module):
         
         layers = []
         
-        # Determinar quantas camadas são necessárias com base no tamanho da imagem
         num_blocks = int(math.log2(img_size)) - 3  # 3 é subtraído porque terminamos com 4x4 (2^2) e queremos começar de img_size
         
-        # Camada inicial
         layers.append(nn.Conv2d(channels_img, features_d, 4, 2, 1))
         layers.append(nn.LeakyReLU(alpha))
         
-        # Blocos intermediários
         in_channels = features_d
         for i in range(num_blocks):
             out_channels = in_channels * 2
             layers.append(self._block(in_channels, out_channels, alpha))
             in_channels = out_channels
         
-        # Camada final
         layers.append(nn.Conv2d(in_channels, 1, 4, 2, 0))
         
         self.disc = nn.Sequential(*layers)
