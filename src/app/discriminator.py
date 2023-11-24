e3842339593783f2c9795fceda936820775ef8ce
Renan Siqueira Antonio <renan.siqu@gmail.com>
2023-11-24 13:51:12 -0300

    feat: Improving discriminator architecture

--- src/app/discriminator.py
+++ src/app/discriminator.py
@@ -1,5 +1,6 @@
 import math
 import torch.nn as nn
+import torch.nn.utils.spectral_norm as spectral_norm
 
 class Discriminator(nn.Module):
     def __init__(self, channels_img, features_d, alpha, img_size):
@@ -9,7 +10,7 @@ class Discriminator(nn.Module):
         
         num_blocks = int(math.log2(img_size)) - 3
         
-        layers.append(nn.Conv2d(channels_img, features_d, 4, 2, 1))
+        layers.append(spectral_norm(nn.Conv2d(channels_img, features_d, 4, 2, 1)))
         layers.append(nn.LeakyReLU(alpha))
         
         in_channels = features_d
@@ -18,15 +19,15 @@ class Discriminator(nn.Module):
             layers.append(self._block(in_channels, out_channels, alpha))
             in_channels = out_channels
 
-        layers.append(nn.Conv2d(in_channels, in_channels * 2, 4, 2, 1))
+        layers.append(spectral_norm(nn.Conv2d(in_channels, in_channels * 2, 4, 2, 1)))
         layers.append(nn.LeakyReLU(alpha))
-        layers.append(nn.Conv2d(in_channels * 2, 1, 4, 2, 0))
+        layers.append(spectral_norm(nn.Conv2d(in_channels * 2, 1, 4, 2, 0)))
         
         self.disc = nn.Sequential(*layers)
 
     def _block(self, in_channels, out_channels, alpha):
         return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 4, 2, 1, bias=False),
+            spectral_norm(nn.Conv2d(in_channels, out_channels, 4, 2, 1, bias=False)),
             nn.BatchNorm2d(out_channels),
             nn.LeakyReLU(alpha)
         )
